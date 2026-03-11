9c2d8ffb99b258f2ed9fa57da22dcfd89d758478
juliana <julidiossa20@gmail.com>
2026-03-11 11:29:17 +0100

    chore(structure):first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header className='header'>
+        <h1 className='logo'>Juliana</h1>
+
+        <nav className='navegation'>
+          <a href='#about'>About</a>
+          <a href='#projects'>Projects</a>
+          <a href='#contact'>Contact</a>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default App
